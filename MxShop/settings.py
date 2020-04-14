05995c5f4bb43d3cafaec70d80bb6a8349d26d98
skylor <739230854@qq.com>
2020-04-14 21:39:08 +0800

    models finish

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = 'v!w6%5y26syg34ner)0ly)z#n=ewd2otax)6%awvx^fys=*eri'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+AUTH_USER_MODEL = 'users.UserProfile'
 
 
 # Application definition
@@ -40,6 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'trade.apps.TradeConfig',
+    'goods.apps.GoodsConfig',
+    'user_operation.apps.UserOperationConfig',
+    'DjangoUeditor',
+    'crispy_forms',
+    'xadmin.apps.XAdminConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -110,15 +119,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
